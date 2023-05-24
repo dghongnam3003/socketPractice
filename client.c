a28ad1688b17d9a9553a6342a1fe15a9c9914468
dghongnam3003 <dghongnam3003>
2023-05-24 08:20:33 +0000

    modify

--- client.c
+++ client.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <arpa/inet.h>
+#include <arpa/inet.h>  
 
 int main(int argc, char *argv[]) 
 {
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
 
     int status, valread, client_fd;
     struct sockaddr_in serv_addr;
-    char* hello = "Hello from client";
+    char* hello = "Den gio roi";
     char buffer[1024] = { 0 };
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
--- server.c
+++ server.c
@@ -6,11 +6,21 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros 
+#include <errno.h>
+
+#define TRUE 1
+#define FALSE 0
+#define INFINITE 100000
 
 int main(int argc, char *argv[]) {
     int opt;
     char *root = NULL;
     int port = 0;
+    char *ip;
 
     const struct option long_options[] = {
         {"help", no_argument, NULL, 'h'},
@@ -29,7 +39,7 @@ int main(int argc, char *argv[]) {
                 break;
             case 'p':
                 port = atoi(optarg);
-                printf("[*] Start file server successfully in 0.0.0.0:%s\n", optarg);
+                ip = optarg;
                 break;
             case '?':
                 printf("Unknown option: %s\n", argv[optind-1]);
@@ -43,12 +53,24 @@ int main(int argc, char *argv[]) {
     }
     
 
-    int server_fd, new_socket, valread;
+    int server_fd, new_socket, valread, client_socket[INFINITE], max_clients = INFINITE, activity, sd;
+    int max_sd;
     struct sockaddr_in address;
     int option = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = { 0 };
-    char* hello = "Hello from server";
+
+    //set socket descriptors
+    fd_set readfds;
+
+
+    char* hello = "Chuan bi di ve thoi";
+
+    //initialise all client_socket[] to 0 so not checked 
+    for (int i = 0; i < max_clients; i++)  
+    {  
+        client_socket[i] = 0;  
+    }
 
 
     // Creating socket file descriptor
@@ -79,7 +101,114 @@ int main(int argc, char *argv[]) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket
+
+    puts("Waiting for connection...");
+
+    while(TRUE) {
+        //clear the socket set
+        FD_ZERO(&readfds);
+
+        //add server_fd to set
+        FD_SET(server_fd, &readfds);
+        max_sd = server_fd;
+
+        //add child socket to set
+        for (int i = 0; i < max_clients; i++) {
+            //socket descriptor
+            sd = client_socket[i];
+
+            //add to the list if valid
+            if (sd > 0) FD_SET(sd, &readfds);
+
+            if (sd > max_sd) max_sd = sd;
+        }
+
+        //wait for an activity on one of the sockets , timeout is NULL , 
+        //so wait indefinitely
+        activity = select( max_sd + 1, &readfds, NULL, NULL, NULL);
+
+        if ((activity < 0) && (errno!=EINTR))  
+        {  
+            printf("select error");  
+        }
+
+        //If something happened on the server_fd socket , 
+        //then its an incoming connection
+        if (FD_ISSET(server_fd, &readfds)) {
+            if ((new_socket = accept(server_fd, 
+                    (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)  
+            {  
+                perror("accept");  
+                exit(EXIT_FAILURE);  
+            }
+
+            //inform user of socket number - used in send and receive commands 
+            printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+            printf("[*] Start file server successfully in 0.0.0.0:%s\n", ip);
+
+
+            //take client ip
+            char client_ip[INET_ADDRSTRLEN];
+            struct sockaddr_in* addr_in = (struct sockaddr_in*)&address;
+            inet_ntop(AF_INET, &(addr_in->sin_addr), client_ip, INET_ADDRSTRLEN);
+            printf("Client IP address: %s\n", client_ip);
+
+
+            //send message Hello from server
+            valread = read(new_socket, buffer, 1024);
+            printf("%s\n", buffer);
+            send(new_socket, hello, strlen(hello), 0);
+            printf("Hello message sent\n");
+
+            //add new socket to array of sockets 
+            for (int i = 0; i < max_clients; i++)  
+            {  
+                //if position is empty 
+                if( client_socket[i] == 0 )  
+                {  
+                    client_socket[i] = new_socket;  
+                    printf("Adding to list of sockets as %d\n" , i);  
+                         
+                    break;  
+                }  
+            }
+        }
+
+        //else its some IO operation on some other socket
+        for (int i = 0; i < max_clients; i++) {
+            sd = client_socket[i];
+
+            if(FD_ISSET(sd, &readfds)) {
+                //Check if it was for closing , and also read the 
+                //incoming message 
+                if ((valread = read( sd , buffer, 1024)) == 0)  
+                {  
+                    //Somebody disconnected , get his details and print 
+                    getpeername(sd , (struct sockaddr*)&address , \
+                        (socklen_t*)&addrlen);  
+                    printf("Host disconnected , ip %s , port %d \n" , 
+                          inet_ntoa(address.sin_addr) , ntohs(address.sin_port));  
+                         
+                    //Close the socket and mark as 0 in list for reuse 
+                    close( sd );  
+                    client_socket[i] = 0;  
+                }  
+                     
+                //Echo back the message that came in 
+                else 
+                {  
+                    //set the string terminating NULL byte on the end 
+                    //of the data read 
+                    valread = read(sd, buffer, 1024);
+                    printf("%s\n", buffer);
+                    send(sd, hello, strlen(hello), 0);
+                    printf("Hello message sent\n"); 
+                }
+            }
+        }
+    }
+
+    /*if ((new_socket
          = accept(server_fd, (struct sockaddr*)&address,
                   (socklen_t*)&addrlen))
         < 0) {
@@ -100,7 +229,7 @@ int main(int argc, char *argv[]) {
     // closing the connected socket
     close(new_socket);
     // closing the listening socket
-    shutdown(server_fd, SHUT_RDWR);
+    shutdown(server_fd, SHUT_RDWR);*/
     
 
     return 0;
